3eb673aef9dff47a2891bb9677d4976eee45bf68
Alex <alexandrulapshin@gmail.com>
2022-09-29 17:39:57 -0400

    Create EmployeeService that delegates the calls to EmployeeDAO

---
+++ cruddemo/src/main/java/com/projects/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.projects.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.projects.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void delete(int theId);
+	
+}
