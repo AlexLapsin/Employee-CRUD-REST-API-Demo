57067dd3d91edb90cc020afd5404727741b89dee
Alex <alexandrulapshin@gmail.com>
2022-09-29 18:22:52 -0400

    Create Rest Controller Methods to FindById and Add Employee

--- cruddemo/src/main/java/com/projects/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/projects/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projects.springboot.cruddemo.dao.EmployeeDAO;
 import com.projects.springboot.cruddemo.entity.Employee;
 import com.projects.springboot.cruddemo.service.EmployeeService;
 
@@ -26,5 +28,28 @@ public class EmployeeRestController {
 	@GetMapping("/employees")
 	public List<Employee> findAll(){
 		return employeeService.findAll();
+	}
+
+	// Add mapping for GET "/employees/{employeeId}"
+	@GetMapping("/employees/{employeeId}")
+	public Employee findById(@PathVariable int theId){
+		Employee theEmployee = employeeService.findById(theId);
+		
+		if(theEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + theId);
+		}
+		
+		return theEmployee;
+	}
+	
+	// add mapping for POST "/employees" - add a new employee
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		// this is to force save of new item instead of update
+		theEmployee.setId(0);
+		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
 	}
 }
