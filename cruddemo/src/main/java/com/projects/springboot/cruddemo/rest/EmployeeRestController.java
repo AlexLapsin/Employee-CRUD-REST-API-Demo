51f7beccc473c44707049fed3e9966cd1f2da6c8
Alex <alexandrulapshin@gmail.com>
2022-09-29 18:36:49 -0400

    Add Rest Controller methods to Update and Delete an Employee

--- cruddemo/src/main/java/com/projects/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/projects/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,11 @@ package com.projects.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,12 +26,14 @@ public class EmployeeRestController {
 		this.employeeService = theEmployeeService;
 	}
 	
+	
 	// expose "/employees" and return list of all employees
 	@GetMapping("/employees")
 	public List<Employee> findAll(){
 		return employeeService.findAll();
 	}
 
+	
 	// Add mapping for GET "/employees/{employeeId}"
 	@GetMapping("/employees/{employeeId}")
 	public Employee findById(@PathVariable int theId){
@@ -42,6 +46,7 @@ public class EmployeeRestController {
 		return theEmployee;
 	}
 	
+	
 	// add mapping for POST "/employees" - add a new employee
 	@PostMapping("/employees")
 	public Employee addEmployee(@RequestBody Employee theEmployee) {
@@ -52,4 +57,35 @@ public class EmployeeRestController {
 		
 		return theEmployee;
 	}
+	
+	
+	// add mapping for PUT "/employees" - update an employee
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+	}
+
+	
+	// add mapping for DELETE "/employees/{employeeId}" - delete an employee
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee theEmployee = employeeService.findById(employeeId);
+		
+		if(theEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		
+		employeeService.delete(employeeId);
+		
+		return "Deleted employee id - " + employeeId;
+	}
+
+	
+
+	
+	
 }
